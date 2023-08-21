f396bdf4e795fab8fd1a90d91f7a850430c4b560
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-08-21 22:07:52 +0700

    style: prettier formatting

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -114,5 +114,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
