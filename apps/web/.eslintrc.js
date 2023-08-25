7acbcc55acc208bf00d96060b036bf22ac59f04c
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-08-25 13:23:29 +0700

    style: prettier formatting

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
