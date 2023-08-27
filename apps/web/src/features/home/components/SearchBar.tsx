d58f492b101f9c6ce9de1bd0afcf49641e4db1c8
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-08-27 21:29:47 +0700

    feat(theme): add dark mode toggle

--- apps/web/src/features/home/components/SearchBar.tsx
+++ apps/web/src/features/home/components/SearchBar.tsx
@@ -1,18 +1,35 @@
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { MagnifyingGlassIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
 
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 
 export const SearchBar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      return;
+    }
+
+    setTheme('light');
+  };
+
   return (
     <div>
-      <div className="flex">
-        <Button variant="link" className="mr-4 font-heading font-extrabold">
+      <div className="flex gap-4">
+        <Button variant="link" className="font-heading font-extrabold">
           V6 Academy
         </Button>
-        <Input className=" rounded-e-none px-6" />
-        <Button className=" rounded-s-none ">
-          <MagnifyingGlassIcon height={20} width={20} />
+        <div className="flex flex-1">
+          <Input className=" rounded-e-none px-6" />
+          <Button className=" rounded-s-none">
+            <MagnifyingGlassIcon height={20} width={20} />
+          </Button>
+        </div>
+        <Button onClick={toggleTheme}>
+          {theme === 'light' ? <SunIcon /> : <MoonIcon />}
         </Button>
       </div>
     </div>
