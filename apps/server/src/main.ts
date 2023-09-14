43374e9e64d4ddb18b9b064b5136236685de954f
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-09-15 00:36:38 +0700

    feat(cors): v6 academy cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,7 +9,12 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://v6-academy-web.vercel.app',
+      'https://v6-academy.com',
+      'https://www.v6-academy.com',
+    ],
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
